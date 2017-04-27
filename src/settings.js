a3a6ada9f4deb93dd4a9f6f1698862a7e7450fc2
Isaac Cambron <isaac@isaaccambron.com>
2017-04-26 21:24:34 -0400

    fix setings docs

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,40 @@
 import { LocalZone } from './zones/localZone';
 
+let now = () => new Date().valueOf(), defaultZone = LocalZone.instance;
+
 /**
  * Global settings
  */
-export class Settings {}
+export class Settings {
+  /**
+   * Get the callback for returning the current timestamp.
+   * @type {function}
+   */
+  static get now() {
+    return now;
+  }
 
-/**
- * Callback for returning the current timestamp.
- */
-Settings.now = () => new Date().valueOf();
+  /**
+   * Set the callback for returning the current timestamp.
+   * @type {function}
+   */
+  static set now(n) {
+    now = n;
+  }
 
-/**
- * The default time zone to create DateTimes in.
- */
-Settings.defaultZone = LocalZone.instance;
+  /**
+   * Get the default time zone to create DateTimes in.
+   * @type {Zone}
+   */
+  static get defaultZone() {
+    return defaultZone;
+  }
+
+  /**
+   * Set the default time zone to create DateTimes in.
+   * @type {Zone}
+   */
+  static set defaultZone(z) {
+    defaultZone = z;
+  }
+}
