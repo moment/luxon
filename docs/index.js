c12461a3c042140134da885478902c7470c9d10d
Isaac Cambron <isaac@isaaccambron.com>
2017-11-27 17:12:08 -0500

    goldplate docs/index.js

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,11 @@
-// This is the table of contents, which exists just to create an ordering.
-// If you're adding a doc, all you have to do is add it here.
+// This file exports a config object to be passed to ESDoc, including what files to add to the manual and what
+// options to set when processing the docstrings and computing documentation coverage.
 
 const path = require('path'),
   fs = require('fs');
 
+// This is the table of contents, which exists just to create an ordering.
+// If you're adding a doc, all you have to do is add it here.
 const manualFiles = [
   'install',
   'tour',
@@ -17,13 +19,11 @@ const manualFiles = [
   'matrix',
   'moment',
   'why'
-];
+].map(d => `./docs/${d}.md`);
 
-function xform(docs) {
-  // paths are relative to root
-  return docs.map(d => `./docs/${d}.md`);
-}
-
+// List all the src subdirectories and generate an exclusion string for each.
+// This is because we assume all the stuff in subdirs is implementation stuff
+// that doesn't need documented.
 function excludeNonRoot() {
   return fs
     .readdirSync('./src')
@@ -34,6 +34,7 @@ function excludeNonRoot() {
     .map(name => `${name}/.*\\.js`);
 }
 
+// also exclude these; there's nothing to document
 const excludeDumb = ['luxon\\.js', 'luxonFilled\\.js'];
 
 module.exports = {
@@ -50,16 +51,18 @@ module.exports = {
         manual: {
           globalIndex: true,
           index: './docs/index.md',
-          files: xform(manualFiles).concat(['./changelog.md', './contributing.md'])
+          files: manualFiles.concat(['./changelog.md', './contributing.md'])
         }
       }
     },
+    // don't consider random unexported functions to be undocumented
     {
       name: 'esdoc-coverage-plugin',
       option: {
         kind: ['class', 'method', 'member', 'get', 'set']
       }
     },
+    // customizations to the ESDoc output
     { name: './docs/plugin.js' }
   ]
 };
