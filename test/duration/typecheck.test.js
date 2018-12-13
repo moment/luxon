46fce9dfbe427d8cb372221267954703d196f1f5
Vincenzo Carrese <v.carrese.dev@gmail.com>
2018-12-13 16:30:00 +0100

    Duration.isDuration now returns false instead of undefined

---
+++ test/duration/typecheck.test.js
@@ -0,0 +1,23 @@
+/* global test expect */
+
+import { Duration } from "../../src/luxon";
+
+//------
+// #isDuration
+//-------
+test("Duration#isDuration return true for valid duration", () => {
+  const dur = Duration.fromObject({ hours: 1 });
+  expect(Duration.isDuration(dur)).toBe(true);
+});
+
+test("Duration#isDuration return true for invalid duration", () => {
+  const dur = Duration.invalid("because");
+  expect(Duration.isDuration(dur)).toBe(true);
+});
+
+test("Duration#isDuration return false for primitives", () => {
+  expect(Duration.isDuration({})).toBe(false);
+  expect(Duration.isDuration({ hours: 60 })).toBe(false);
+  expect(Duration.isDuration(1)).toBe(false);
+  expect(Duration.isDuration("")).toBe(false);
+});
