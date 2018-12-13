3e55ad5b485a8fe9eb00a7be3c9c5efe24e7f32c
Vincenzo Carrese <v.carrese.dev@gmail.com>
2018-12-13 16:28:28 +0100

    Datetime.isDateTime now returns false instead of undefined

---
+++ test/datetime/typecheck.test.js
@@ -0,0 +1,22 @@
+/* global test expect */
+import { DateTime } from "../../src/luxon";
+
+//------
+// #isDateTime()
+//------
+test("DateTime#isDateTime return true for valid DateTime", () => {
+  const dt = DateTime.local();
+  expect(DateTime.isDateTime(dt)).toBe(true);
+});
+
+test("DateTime#isDateTime return true for invalid DateTime", () => {
+  const dt = DateTime.invalid("because");
+  expect(DateTime.isDateTime(dt)).toBe(true);
+});
+
+test("DateTime#isDateTime return false for primitives", () => {
+  expect(DateTime.isDateTime({})).toBe(false);
+  expect(DateTime.isDateTime({ hours: 60 })).toBe(false);
+  expect(DateTime.isDateTime(1)).toBe(false);
+  expect(DateTime.isDateTime("")).toBe(false);
+});
