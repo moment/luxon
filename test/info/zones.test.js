205a7cf48e1fc328269959000fcb08dbe670c9c1
Daniel Wolf <dwolf@dannad.de>
2019-01-10 13:58:47 +0100

    Add Info.normalizeZone
    Closes #369

--- test/info/zones.test.js
+++ test/info/zones.test.js
@@ -1,6 +1,6 @@
 /* global test expect */
 
-import { Info } from "../../src/luxon";
+import { Info, FixedOffsetZone, IANAZone, InvalidZone, LocalZone, Settings } from "../../src/luxon";
 import { Helpers } from "../helpers";
 
 //------
@@ -55,4 +55,45 @@ test("Info.isValidIANAZone returns true for valid zones like America/Indiana/Ind
 
 test("Info.isValidIANAZone returns false for well-specified but invalid zones like America/Indiana/Blork", () => {
   expect(Info.isValidIANAZone("America/Indiana/Blork")).toBe(false);
+});
+
+//------
+// .normalizeZone()
+//------
+
+test("Info.normalizeZone returns Zone objects unchanged", () => {
+  const fixedOffsetZone = FixedOffsetZone.instance(5);
+  expect(Info.normalizeZone(fixedOffsetZone)).toBe(fixedOffsetZone);
+
+  const ianaZone = new IANAZone("Europe/Paris");
+  expect(Info.normalizeZone(ianaZone)).toBe(ianaZone);
+
+  const invalidZone = new InvalidZone("bumblebee");
+  expect(Info.normalizeZone(invalidZone)).toBe(invalidZone);
+
+  const localZone = LocalZone.instance;
+  expect(Info.normalizeZone(localZone)).toBe(localZone);
+});
+
+test.each([
+  ["Local", LocalZone.instance],
+  ["UTC", FixedOffsetZone.utcInstance],
+  ["GMT", FixedOffsetZone.utcInstance],
+  ["Etc/GMT+5", FixedOffsetZone.instance(-5 * 60)],
+  ["Etc/GMT-10", FixedOffsetZone.instance(+10 * 60)],
+  ["Europe/Paris", new IANAZone("Europe/Paris")],
+  [0, FixedOffsetZone.utcInstance],
+  [3, FixedOffsetZone.instance(3)],
+  [-11, FixedOffsetZone.instance(-11)]
+])("Info.normalizeZone converts valid input %p into valid Zone instance", (input, expected) => {
+  expect(Info.normalizeZone(input)).toEqual(expected);
+});
+
+test("Info.normalizeZone converts unknown name to invalid Zone", () => {
+  expect(Info.normalizeZone("bumblebee").isValid).toBe(false);
+});
+
+test("Info.normalizeZone converts null and undefined to default Zone", () => {
+  expect(Info.normalizeZone(null)).toBe(Settings.defaultZone);
+  expect(Info.normalizeZone(undefined)).toBe(Settings.defaultZone);
 });
