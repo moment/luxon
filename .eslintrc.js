c81d497dd02910bd2ff53afcc2627a988590d594
Gilles Debunne <gilles.debunne@gmail.com>
2019-03-07 09:26:27 +0100

    Remove prettier rule, implied by `recommended`

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ["standard", "plugin:prettier/recommended"],
   plugins: ["import", "prettier", "babel"],
   rules: {
-    "prettier/prettier": "error",
     "linebreak-style": "off",
     "one-var": "off",
     "spaced-comment": ["error", "always", { exceptions: ["-"] }],
