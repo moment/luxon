df27f7aaf45097380b19170dadee62506ed11977
Roma Caraus <roman.caraus@codefactorygroup.com>
2019-03-27 10:45:42 +0200

    Add test for isValidZone

--- test/impl/util.test.js
+++ test/impl/util.test.js
@@ -1,5 +1,5 @@
 /* global test expect, jest */
-import { memoize } from "../../src/impl/util";
+import { isValidZone, memoize } from "../../src/impl/util";
 
 test("memoize calls target function once for same argument", () => {
   const fn = jest.fn();
@@ -15,4 +15,10 @@ test("memoize calls target function if arguments are different", () => {
   memoized("test 1");
   memoized("test 2");
   expect(fn).toBeCalledTimes(2);
+});
+
+test("isValidZone should return correct results", () => {
+  expect(isValidZone("America/Los_Angeles")).toBe(true);
+  expect(isValidZone("Europe/London")).toBe(true);
+  expect(isValidZone("Invalid/Zone")).toBe(false);
 });
