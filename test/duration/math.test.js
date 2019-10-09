df59f6737f0062ee6793b595f33143a28ce86b8f
Isaac Cambron <isaac@isaaccambron.com>
2019-10-09 01:18:01 -0400

    Add Duration#mapUnits (#592)

--- test/duration/math.test.js
+++ test/duration/math.test.js
@@ -137,3 +137,38 @@ test("Duration#negate preserves conversionAccuracy", () => {
     result = dur.negate();
   expect(result.conversionAccuracy).toBe("longterm");
 });
+
+//------
+// #mapUnits
+//------
+
+test("Duration#units can multiply durations", () => {
+  const dur = Duration.fromObject({ hours: 1, minutes: 2, seconds: -3, milliseconds: -4 }),
+    result = dur.mapUnits(x => x * 5);
+
+  expect(result.hours).toBe(5);
+  expect(result.minutes).toBe(10);
+  expect(result.seconds).toBe(-15);
+  expect(result.milliseconds).toBe(-20);
+});
+
+test("Duration#units can take the unit into account", () => {
+  const dur = Duration.fromObject({ hours: 1, minutes: 2, seconds: -3, milliseconds: -4 }),
+    result = dur.mapUnits((x, u) => x * (u === "milliseconds" ? 2 : 5));
+
+  expect(result.hours).toBe(5);
+  expect(result.minutes).toBe(10);
+  expect(result.seconds).toBe(-15);
+  expect(result.milliseconds).toBe(-8);
+});
+
+test("Duration#mapUnits maintains invalidity", () => {
+  const dur = Duration.invalid("because").mapUnits(x => x * 5);
+  expect(dur.isValid).toBe(false);
+  expect(dur.invalidReason).toBe("because");
+});
+
+test("Duration#mapUnits requires that fn return a number", () => {
+  const dur = Duration.fromObject({ hours: 1, minutes: 2, seconds: -3, milliseconds: -4 });
+  expect(() => dur.mapUnits(() => "hello?")).toThrow();
+});
