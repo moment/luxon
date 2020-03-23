e1a32db68f7eefa56b9a6519c3bd7ed030206bea
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-03-23 12:32:26 -0700

    Address review comment

--- src/datetime.js
+++ src/datetime.js
@@ -556,7 +556,7 @@ export default class DateTime {
   static fromMillis(milliseconds, options = {}) {
     if (!isNumber(milliseconds)) {
       throw new InvalidArgumentError(
-        "fromMillis requires a numerical input, but received " + milliseconds
+        `fromMillis requires a numerical input, but received a ${typeof milliseconds} with value ${milliseconds}`
       );
     } else if (milliseconds < -MAX_DATE || milliseconds > MAX_DATE) {
       // this isn't perfect because because we can still end up out of range because of additional shifting, but it's a start
