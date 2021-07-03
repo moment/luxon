9dcec8c32f76d9dac295d73b42e3958643a883ae
Isaac Cambron <isaac@isaaccambron.com>
2021-07-03 13:05:51 -0400

    bump to 1.28.0

--- src/luxon.js
+++ src/luxon.js
@@ -9,7 +9,7 @@ import InvalidZone from "./zones/invalidZone.js";
 import LocalZone from "./zones/localZone.js";
 import Settings from "./settings.js";
 
-const VERSION = "1.27.0";
+const VERSION = "1.28.0";
 
 export {
   VERSION,
