d95ddf12b3d9b116054a8c715c4aa596c4a34454
Dasa Paddock <dpaddock@esri.com>
2021-08-26 07:39:39 -0700

    Cleanup Info.features doc and example (#1008)

--- src/info.js
+++ src/info.js
@@ -157,10 +157,10 @@ export default class Info {
 
   /**
    * Return the set of available features in this environment.
-   * Some features of Luxon are not available in all environments. For example, on older browsers, timezone support is not available. Use this function to figure out if that's the case.
+   * Some features of Luxon are not available in all environments. For example, on older browsers, relative time formatting support is not available. Use this function to figure out if that's the case.
    * Keys:
    * * `relative`: whether this environment supports relative time formatting
-   * @example Info.features() //=> { intl: true, intlTokens: false, zones: true, relative: false }
+   * @example Info.features() //=> { relative: false }
    * @return {Object}
    */
   static features() {
