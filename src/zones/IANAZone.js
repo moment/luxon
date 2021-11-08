1d43d4689b8b5e81452422e4c4a4575cb7645ee3
Isaac Cambron <isaac@isaaccambron.com>
2021-11-08 16:43:51 -0500

    switch back to using hour12 for IANA offset calculation

--- src/zones/IANAZone.js
+++ src/zones/IANAZone.js
@@ -7,7 +7,7 @@ let dtfCache = {};
 function makeDTF(zone) {
   if (!dtfCache[zone]) {
     dtfCache[zone] = new Intl.DateTimeFormat("en-US", {
-      hourCycle: "h23",
+      hour12: false,
       timeZone: zone,
       year: "numeric",
       month: "2-digit",
@@ -152,11 +152,14 @@ export default class IANAZone extends Zone {
         ? partsOffset(dtf, date)
         : hackyOffset(dtf, date);
 
+    // because we're using hour12 and https://bugs.chromium.org/p/chromium/issues/detail?id=1025564&can=2&q=%2224%3A00%22%20datetimeformat
+    const adjustedHour = hour === 24 ? 0 : hour;
+
     const asUTC = objToLocalTS({
       year,
       month,
       day,
-      hour,
+      hour: adjustedHour,
       minute,
       second,
       millisecond: 0,
