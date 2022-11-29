moment/luxon

commit d6ea633661b606333286b12bd04db3391dd7a353

Add Settings.twoDigitCutoffYear to configure parsing 'yy' token. (#1330)

src/impl/util.js
@@ -5,6 +5,7 @@
 */
 
 import { InvalidArgumentError } from "../errors.js";
+import Settings from "../settings.js";
 
 /**
  * @private
@@ -185,7 +186,7 @@ export function weeksInWeekYear(weekYear) {
 export function untruncateYear(year) {
   if (year > 99) {
     return year;
-  } else return year > 60 ? 1900 + year : 2000 + year;
+  } else return year > Settings.twoDigitCutoffYear ? 1900 + year : 2000 + year;
 }
 
 // PARSING

test/datetime/tokenParse.test.js
@@ -136,9 +136,22 @@ test("DateTime.fromFormat() with yyyyyy strictly parses extended years", () => {
 });
 
 test("DateTime.fromFormat() defaults yy to the right century", () => {
-  expect(DateTime.fromFormat("55", "yy").year).toBe(2055);
-  expect(DateTime.fromFormat("70", "yy").year).toBe(1970);
-  expect(DateTime.fromFormat("1970", "yy").year).toBe(1970);
+  expect(DateTime.fromFormat("60", "yy").year).toBe(2060);
+  expect(DateTime.fromFormat("61", "yy").year).toBe(1961);
+  expect(DateTime.fromFormat("1960", "yy").year).toBe(1960);
+});
+
+test("DateTime.fromFormat() respects Settings.twoDigitCutoffYear when parsing yy to the right century", () => {
+  const oldTwoDigitCutoffYear = Settings.twoDigitCutoffYear;
+
+  try {
+    Settings.twoDigitCutoffYear = 50;
+    expect(DateTime.fromFormat("50", "yy").year).toBe(2050);
+    expect(DateTime.fromFormat("51", "yy").year).toBe(1951);
+    expect(DateTime.fromFormat("1950", "yy").year).toBe(1950);
+  } finally {
+    Settings.twoDigitCutoffYear = oldTwoDigitCutoffYear;
+  }
 });
 
 test("DateTime.fromFormat() parses hours", () => {
@@ -415,6 +428,25 @@ test("DateTime.fromFormat() accepts weekYear by itself", () => {
   expect(d.weekYear).toBe(2004);
   expect(d.weekNumber).toBe(1);
   expect(d.weekday).toBe(1);
+});
+
+test("DateTime.fromFormat() defaults kk to the right century", () => {
+  expect(DateTime.fromFormat("60", "kk").weekYear).toBe(2060);
+  expect(DateTime.fromFormat("61", "kk").weekYear).toBe(1961);
+  expect(DateTime.fromFormat("1960", "kk").weekYear).toBe(1960);
+});
+
+test("DateTime.fromFormat() respects Settings.twoDigitCutoffYear when parsing two digit weekYear to the right century", () => {
+  const oldTwoDigitCutoffYear = Settings.twoDigitCutoffYear;
+
+  try {
+    Settings.twoDigitCutoffYear = 50;
+    expect(DateTime.fromFormat("50", "kk").weekYear).toBe(2050);
+    expect(DateTime.fromFormat("51", "kk").weekYear).toBe(1951);
+    expect(DateTime.fromFormat("1950", "kk").weekYear).toBe(1950);
+  } finally {
+    Settings.twoDigitCutoffYear = oldTwoDigitCutoffYear;
+  }
 });
 
 test("DateTime.fromFormat() accepts weekNumber by itself", () => {

src/settings.js
@@ -9,6 +9,7 @@ let now = () => Date.now(),
   defaultLocale = null,
   defaultNumberingSystem = null,
   defaultOutputCalendar = null,
+  twoDigitCutoffYear = 60,
   throwOnInvalid;
 
 /**
@@ -98,6 +99,26 @@ export default class Settings {
    */
   static set defaultOutputCalendar(outputCalendar) {
     defaultOutputCalendar = outputCalendar;
+  }
+
+  /**
+   * Get the cutoff year after which a string encoding a year as two digits is interpreted to occur in the current century.
+   * @type {number}
+   */
+  static get twoDigitCutoffYear() {
+    return twoDigitCutoffYear;
+  }
+
+  /**
+   * Set the cutoff year after which a string encoding a year as two digits is interpreted to occur in the current century.
+   * @type {number}
+   * @example Settings.twoDigitCutoffYear = 0 // cut-off year is 0, so all 'yy' are interpretted as current century
+   * @example Settings.twoDigitCutoffYear = 50 // '49' -> 1949; '50' -> 2050
+   * @example Settings.twoDigitCutoffYear = 1950 // interpretted as 50
+   * @example Settings.twoDigitCutoffYear = 2050 // ALSO interpretted as 50
+   */
+  static set twoDigitCutoffYear(cutoffYear) {
+    twoDigitCutoffYear = cutoffYear % 100;
   }
 
   /**

commit 3858a2aa4b5f270351a2fe0df216a1c3e3dd7216

Fix tests (#1336)
* Fix tokenParse tests import broken by d6ea633

Co-authored-by: dobon <dobon@users.noreply.github.com>

src/impl/regexParser.js
@@ -3,7 +3,6 @@ import {
   signedOffset,
   parseInteger,
   parseMillis,
-  ianaRegex,
   isUndefined,
   parseFloating,
 } from "./util.js";
@@ -20,6 +19,8 @@ import IANAZone from "../zones/IANAZone.js";
  * combineExtractors() does the work of combining them, keeping track of the cursor through multiple extractions.
  * Some extractions are super dumb and simpleParse and fromStrings help DRY them.
  */
+
+const ianaRegex = /[A-Za-z_+-]{1,256}(?::?\/[A-Za-z0-9_+-]{1,256}(?:\/[A-Za-z0-9_+-]{1,256})?)?/;
 
 function combineRegexes(...regexes) {
   const full = regexes.reduce((f, r) => f + r.source, "");

src/impl/util.js
@@ -269,6 +269,3 @@ export function formatOffset(offset, format) {
 export function timeObject(obj) {
   return pick(obj, ["hour", "minute", "second", "millisecond"]);
 }
-
-export const ianaRegex =
-  /[A-Za-z_+-]{1,256}(?::?\/[A-Za-z0-9_+-]{1,256}(?:\/[A-Za-z0-9_+-]{1,256})?)?/;

test/datetime/tokenParse.test.js
@@ -1,7 +1,7 @@
 /* global test expect */
 import { DateTime } from "../../src/luxon";
 import Helpers from "../helpers";
-
+import Settings from "../../src/settings";
 import { ConflictingSpecificationError } from "../../src/errors";
 
 //------