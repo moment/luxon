af6f3c24eea01f3c8a5c0499db0ceb5876399fa7
jmostaer <jens.mostaert@gmail.com>
2023-07-13 11:18:19 +0200

    Default config DateTime constructor
    Set default value for config in DateTime constructor

--- src/datetime.js
+++ src/datetime.js
@@ -432,7 +432,7 @@ export default class DateTime {
   /**
    * @access private
    */
-  constructor(config) {
+  constructor(config = {}) {
     const zone = config.zone || Settings.defaultZone;
 
     let invalid =
