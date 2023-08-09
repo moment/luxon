9575754ac190cf7bed5776b02f5616d549a6b112
Isaac Cambron <icambron@gmail.com>
2023-08-08 22:34:45 -0400

    bump to 3.4.0

--- src/luxon.js
+++ src/luxon.js
@@ -9,7 +9,7 @@ import InvalidZone from "./zones/invalidZone.js";
 import SystemZone from "./zones/systemZone.js";
 import Settings from "./settings.js";
 
-const VERSION = "3.3.0";
+const VERSION = "3.4.0";
 
 export {
   VERSION,
