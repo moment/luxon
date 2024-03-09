6b0ec2e421cf492122484637d89a85b273db1414
Ben Hughes <benjamin@makenotion.com>
2024-03-09 09:37:23 -0700

    Perf: Memoize digitsRegex (#1581)
    * Memoize digitsRegex
    
    Profiles reveal that a substantial portion of fromFormat time is spent
    in digitsRegex. It is only called from unitsForToken and it is always
    called with 11 suffixes to match different numbers of digits.
    
    There are 21 numbering systems, so a fully expanded cache would hold
    11*21 = 231 regexes.
    
    Benchmarks show about a 1.5x improvement on DateTime.fromFormat
    
    * Allow digitRegex cache to be reset

--- src/impl/digits.js
+++ src/impl/digits.js
@@ -70,6 +70,21 @@ export function parseDigits(str) {
   }
 }
 
+// cache of {numberingSystem: {append: regex}}
+let digitRegexCache = {};
+export function resetDigitRegexCache() {
+  digitRegexCache = {};
+}
+
 export function digitRegex({ numberingSystem }, append = "") {
-  return new RegExp(`${numberingSystems[numberingSystem || "latn"]}${append}`);
+  const ns = numberingSystem || "latn";
+
+  if (!digitRegexCache[ns]) {
+    digitRegexCache[ns] = {};
+  }
+  if (!digitRegexCache[ns][append]) {
+    digitRegexCache[ns][append] = new RegExp(`${numberingSystems[ns]}${append}`);
+  }
+
+  return digitRegexCache[ns][append];
 }
