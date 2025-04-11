a6c5577966f5f071343f0fe35da82b64c09189a4
dobon <dobon@users.noreply.github.com>
2025-04-11 05:40:36 -0400

    Only add minus sign on largest unit of negative durations

--- test/duration/format.test.js
+++ test/duration/format.test.js
@@ -283,6 +283,14 @@ test("Duration#toFormat returns a lame string for invalid durations", () => {
   expect(Duration.invalid("because").toFormat("yy")).toBe("Invalid Duration");
 });
 
+test("Duration#toFormat shows negative sign on the largest unit", () => {
+  expect(Duration.fromObject({ years: -3, seconds: -45 }).toFormat("yyss")).toBe("-0345");
+  expect(
+    Duration.fromObject({ years: -3, seconds: -45 }).toFormat("'before'yy'between'ss'after'")
+  ).toBe("before-03between45after");
+  expect(Duration.fromObject({ years: -3, seconds: -45 }).toFormat("ssyy")).toBe("45-03");
+});
+
 //------
 // #humanize()
 //------
