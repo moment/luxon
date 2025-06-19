55d2dd7c14717efcde06f5b7118a95aa610b0682
dobon <dobon@users.noreply.github.com>
2025-06-19 07:25:41 -0700

    Better toggle positioning and code cleanup

--- site/plugins/dark-theme-toggle.js
+++ site/plugins/dark-theme-toggle.js
@@ -1,103 +1,102 @@
 (function () {
   const darkThemeTogglePlugin = function (hook, vm) {
-    const CHECKBOX_ID = "dark-theme-input";
-    const STORAGE_KEY = "docsify-toggle-dark-theme";
-    localStorage[STORAGE_KEY] ??= matchMedia("(prefers-color-scheme: dark)").matches;
-    const initialValue = localStorage[STORAGE_KEY] === "true";
-    const dom = Docsify.dom;
-    const darkThemeStyleSheet = dom.find('link[href$="dark.css"]');
-    const toggleDarkMode = (isDark) => (darkThemeStyleSheet.disabled = !isDark);
-    const pluginStyleEl = dom.create("style");
-    pluginStyleEl.textContent = `#${CHECKBOX_ID} {
-			display: none;
-		}
-		.dark-theme-label {
-			position: absolute;
-			display: inline-block;
-			width: 52px;
-			height: 28px;
-			margin-left: 2rem;
-			margin-top: 2rem;
-			z-index: 0;
-			cursor: pointer;
-		}
-		.dark-theme-label::before, .dark-theme-label::after {
-			position: absolute;
-			top: 0.1em;
-			font-size: 16px;
-			transition: opacity 0.3s;
-		}
-		.dark-theme-label::after {
-			content: "🌞";
-			right: 0.1em;
-			opacity: 1;
-		}
-		.dark-theme-label::before {
-			content: "🌙";
-			left: 0.1em;
-			opacity: 0;
-			z-index: 1;
-		}
-		.slider {
-			position: absolute;
-			top: 0;
-			left: 0;
-			right: 0;
-			bottom: 0;
-			background-color: var(--theme-color, #42b983);
-			border-radius: 28px;
-			transition: background-color 0.3s;
-		}
-		.slider::before {
-			content: "";
-			position: absolute;
-			height: 22px;
-			width: 22px;
-			left: 3px;
-			top: 3px;
-			z-index: 1;
-			background-color: white;
-			border-radius: 50%;
-			transition: transform 0.3s;
-		}
-		#${CHECKBOX_ID}:checked + .dark-theme-label::after {
-			opacity: 0;
-		}
-		#${CHECKBOX_ID}:checked + .dark-theme-label::before {
-			opacity: 1;
-		}
-		#${CHECKBOX_ID}:checked + .dark-theme-label .slider {
-			background-color: var(--theme-color, #ea6f5a);
-		}
-		#${CHECKBOX_ID}:checked + .dark-theme-label .slider::before {
-			transform: translateX(24px);
-		}
-		#${CHECKBOX_ID}:checked ~ section.cover.show {
-			background-color: rgb(79 58 120) !important;
-		}
-		section.cover.show {
-			z-index: -1;
-		}`;
-    dom.head.appendChild(pluginStyleEl);
-    hook.init(() => toggleDarkMode(initialValue));
-    hook.ready(() => {
-      const body = dom.body;
-      const checkbox = dom.create("input");
-      checkbox.id = CHECKBOX_ID;
-      checkbox.type = "checkbox";
-      checkbox.checked = initialValue;
-      checkbox.addEventListener("change", () => {
-        toggleDarkMode((localStorage[STORAGE_KEY] = checkbox.checked));
-      });
-      const label = dom.create("label");
-      label.setAttribute("for", CHECKBOX_ID);
-      label.className = "dark-theme-label";
-      const slider = dom.create("span");
-      slider.className = "slider";
-      label.appendChild(slider);
-      body.prepend(label);
-      body.prepend(checkbox);
-    });
+    const TOGGLE_ID = `${vm.config.name}-docsify-dark-theme-toggle`,
+      dom = Docsify.dom,
+      darkThemeStyleSheet = dom.find('link[href$="dark.css"]'),
+      toggleDarkTheme = () => {
+        const isDark = localStorage[TOGGLE_ID] = checkbox.checked;
+        darkThemeStyleSheet.disabled = !isDark;
+        dom.toggleClass(dom.body, isDark ? "add" : "remove", "dark");
+      };
+    localStorage[TOGGLE_ID] ??= matchMedia("(prefers-color-scheme: dark)").matches;
+    const toggle = dom.create("label", `<input type="checkbox" ${(localStorage[TOGGLE_ID] === "true") ? 'checked' : ''} /><span />`),
+      checkbox = dom.find(toggle, "input");
+    toggle.id = TOGGLE_ID;
+    dom.on(checkbox, "change", toggleDarkTheme);
+    hook.init(toggleDarkTheme);
+    hook.doneEach(() => dom.before(dom.find('section.cover.show') || dom.find('.sidebar > .app-name'), toggle));
+    dom.style(
+`#${TOGGLE_ID} > input {
+  display: none;
+}
+#${TOGGLE_ID} {
+  position: absolute;
+  display: inline-block;
+  width: 52px;
+  height: 28px;
+  margin-left: 2rem;
+  margin-top: 1.5rem;
+  left: 0;
+  top: 0;
+  z-index: 0;
+  cursor: pointer;
+}
+.sidebar > .app-name {
+  position: relative;
+}
+.app-name > #${TOGGLE_ID} {
+  margin-right: 1rem;
+  margin-top: 0;
+  right: 0;
+  left: unset;
+}
+#${TOGGLE_ID}::before, #${TOGGLE_ID}::after {
+  position: absolute;
+  top: 0.1em;
+  font-size: 16px;
+  transition: opacity 0.3s;
+}
+#${TOGGLE_ID}::before {
+  content: "🌙";
+  left: 0.1em;
+  opacity: 0;
+  z-index: 1;
+}
+#${TOGGLE_ID}::after {
+  content: "🌞";
+  right: 0.1em;
+  opacity: 1;
+}
+#${TOGGLE_ID} > span {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: var(--theme-color, #42b983);
+  border-radius: 28px;
+  transition: background-color 0.3s;
+}
+#${TOGGLE_ID} > span::before {
+  content: "";
+  position: absolute;
+  height: 22px;
+  width: 22px;
+  left: 3px;
+  top: 3px;
+  z-index: 1;
+  background-color: white;
+  border-radius: 50%;
+  transition: transform 0.3s;
+}
+.dark #${TOGGLE_ID}::after {
+  opacity: 0;
+}
+.dark #${TOGGLE_ID}::before {
+  opacity: 1;
+}
+.dark #${TOGGLE_ID} > span {
+  background-color: var(--theme-color, #ea6f5a);
+}
+.dark #${TOGGLE_ID} > span::before {
+  transform: translateX(24px);
+}
+.dark .sidebar li {
+  margin-right: 0;
+}
+.dark section.cover.show {
+  background-color: rgb(79 58 120) !important;
+}`);
   };
 
   $docsify = $docsify || {};
