f5e1f32a07cca53f58f136565373fcb66ec88d20
Take Weiland <diesieben07@gmail.com>
2025-09-27 15:29:38 +0200

    Remove Jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  roots: ["test"],
-  coverageDirectory: "build/coverage",
-  collectCoverageFrom: ["src/**/*.js", "!src/zone.js"],
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
-};
