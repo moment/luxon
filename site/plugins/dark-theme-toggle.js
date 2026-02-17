9056cc142fcdef9c452211220a3868c04c91dd12
camoch23 <144624835+camoch23@users.noreply.github.com>
2026-02-17 17:53:07 -0500

    Relocate dark mode toggle between cover and sidebar based on scroll visibility

--- site/plugins/dark-theme-toggle.js
+++ site/plugins/dark-theme-toggle.js
@@ -14,7 +14,45 @@
     toggleEl.id = TOGGLE_ID;
     dom.on(toggleEl, "click", () => applyTheme(true));
     hook.init(applyTheme);
-    hook.doneEach(() => dom.before(dom.find(".sidebar > .app-name"), toggleEl));
+
+    //hook.doneEach(() => dom.before(dom.find(".sidebar > .app-name"), toggleEl));
+
+    hook.doneEach(() => {
+      const cover = dom.find(".cover.show");
+      const sidebarTarget = dom.find(".sidebar > .app-name");
+
+      if (!sidebarTarget) return;
+
+      const moveToSidebar = () => {
+        dom.before(sidebarTarget, toggleEl);
+      };
+
+      const moveToCover = () => {
+        if (cover) {
+          dom.before(cover, toggleEl);
+        }
+      };
+
+      if (cover) {
+        const observer = new IntersectionObserver(
+          (entries) => {
+            const isVisible = entries[0].isIntersecting;
+
+            if (isVisible) {
+              moveToCover();
+            } else {
+              moveToSidebar();
+            }
+          },
+          { threshold: 0.1 }
+        );
+
+        observer.observe(cover);
+      } else {
+        // On non-home pages
+        moveToSidebar();
+      }
+    });
   };
   $docsify ??= {};
   $docsify.plugins = [...($docsify.plugins ?? []), darkThemeTogglePlugin];
