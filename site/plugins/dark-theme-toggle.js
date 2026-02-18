ae2ccb7f137d14adc2af9b8511a50fd0b82eca98
dobon <dobon@users.noreply.github.com>
2026-02-18 07:06:20 +0000

    code review modifications

--- site/plugins/dark-theme-toggle.js
+++ site/plugins/dark-theme-toggle.js
@@ -3,7 +3,6 @@
     const TOGGLE_ID = "docsify-dark-theme-toggle",
       dom = Docsify.dom,
       darkThemeStyleSheet = dom.find('link[href$="dark.css"]'),
-      toggleEl = dom.create("div", "<span />"),
       applyTheme = (swap = false) => {
         const isDark = Boolean(swap ^ (localStorage[TOGGLE_ID] == "true"));
         localStorage[TOGGLE_ID] = isDark;
@@ -11,45 +10,17 @@
         dom.toggleClass(dom.body, isDark ? "add" : "remove", "dark");
       };
     localStorage[TOGGLE_ID] ??= matchMedia("(prefers-color-scheme: dark)").matches;
-    toggleEl.id = TOGGLE_ID;
-    dom.on(toggleEl, "click", () => applyTheme(true));
     hook.init(applyTheme);
-
     hook.doneEach(() => {
-      const cover = dom.find(".cover.show");
-      const sidebarTarget = dom.find(".sidebar > .app-name");
-
-      if (!sidebarTarget) return;
-
-      const moveToSidebar = () => {
-        dom.before(sidebarTarget, toggleEl);
-      };
-
-      const moveToCover = () => {
-        if (cover) {
-          dom.before(cover, toggleEl);
+      dom.findAll(".cover.show, .sidebar > .app-name").forEach((target) => {
+        let toggleEl = dom.find(target, `.${TOGGLE_ID}`);
+        if (null == toggleEl) {
+          toggleEl = dom.create("div", "<span />");
+          toggleEl.className = TOGGLE_ID;
+          dom.on(toggleEl, "click", () => applyTheme(true));
         }
-      };
-
-      if (cover) {
-        const observer = new IntersectionObserver(
-          (entries) => {
-            const isVisible = entries[0].isIntersecting;
-
-            if (isVisible) {
-              moveToCover();
-            } else {
-              moveToSidebar();
-            }
-          },
-          { threshold: 0.1 }
-        );
-
-        observer.observe(cover);
-      } else {
-        // On non-home pages
-        moveToSidebar();
-      }
+        dom.before(target, toggleEl);
+      });
     });
   };
   $docsify ??= {};
