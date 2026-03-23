651127dc694329d51b56d491c56408e9dc4e6923
dobon <dobon@users.noreply.github.com>
2026-03-23 14:39:45 -0700

    Render dark mode toggle in sidebar on all pages (#1763)
    ---------
    
    Co-authored-by: camoch23 <144624835+camoch23@users.noreply.github.com>
    Co-authored-by: Take Weiland <diesieben07@gmail.com>

--- site/plugins/dark-theme-toggle.js
+++ site/plugins/dark-theme-toggle.js
@@ -3,7 +3,6 @@
     const TOGGLE_ID = "docsify-dark-theme-toggle",
       dom = Docsify.dom,
       darkThemeStyleSheet = dom.find('link[href$="dark.css"]'),
-      toggleEl = dom.create("div", "<span />"),
       applyTheme = (swap = false) => {
         const isDark = Boolean(swap ^ (localStorage[TOGGLE_ID] == "true"));
         localStorage[TOGGLE_ID] = isDark;
@@ -11,10 +10,18 @@
         dom.toggleClass(dom.body, isDark ? "add" : "remove", "dark");
       };
     localStorage[TOGGLE_ID] ??= matchMedia("(prefers-color-scheme: dark)").matches;
-    toggleEl.id = TOGGLE_ID;
-    dom.on(toggleEl, "click", () => applyTheme(true));
     hook.init(applyTheme);
-    hook.doneEach(() => dom.before(dom.find(".cover.show, .sidebar > .app-name"), toggleEl));
+    hook.doneEach(() => {
+      dom.findAll(".cover.show, .sidebar > .app-name").forEach((target) => {
+        let toggleEl = dom.find(target, `.${TOGGLE_ID}`);
+        if (null == toggleEl) {
+          toggleEl = dom.create("div", "<span />");
+          toggleEl.className = TOGGLE_ID;
+          dom.on(toggleEl, "click", () => applyTheme(true));
+        }
+        dom.before(target, toggleEl);
+      });
+    });
   };
   $docsify ??= {};
   $docsify.plugins = [...($docsify.plugins ?? []), darkThemeTogglePlugin];
